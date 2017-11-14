62f1bc5b1acc9910fb54f80620e9b482a6bca089
soledad <mariasolescobar21@gmail.com>
2017-11-14 12:07:47 -0300

    pruebas de avance y retroceso

--- test/primeraEntregaTests/avanceDinamicoTest.java
+++ test/primeraEntregaTests/avanceDinamicoTest.java
@@ -1,7 +1,5 @@
 package primeraEntregaTests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,7 +8,9 @@ import fiuba.algo3.Jugador;
 import fiuba.algo3.Tablero;
 import fiuba.algo3.celdas.Visitable;
 import fiuba.algo3.celdas.especiales.AvanceDinamico;
-import fiuba.algo3.celdas.especiales.RetrocesoDinamico;
+import fiuba.algo3.celdas.especiales.Carcel;
+import fiuba.algo3.celdas.especiales.Policia;
+import fiuba.algo3.celdas.especiales.Quini6;
 
 public class avanceDinamicoTest {
 
@@ -32,4 +32,24 @@ public class avanceDinamicoTest {
 		Visitable resultado = tablero.getPosicionDeJugador(jugador);
 		Assert.assertEquals(esperado, resultado);
 	}
+	@Test
+	public void jugadorCaeEnAvanceDinamicoSumando9SuPosicionDeberiaAvanzar1() {
+		Tablero tablero = new Tablero();
+		AvanceDinamico avanceDinamico = new AvanceDinamico();
+		Jugador jugador = new Jugador();
+		tablero.agregarCelda(avanceDinamico);
+		tablero.agregarJugador(jugador);
+		Dado dado1 = new Dado();
+		Dado dado2 = new Dado();
+		dado1.setValor(1);
+		dado2.setValor(8);
+		jugador.setValorDeTiro(dado1.getValor() + dado2.getValor());
+		Visitable esperado = tablero.getPosicionDeJugador(jugador);
+		tablero.avanzarJugador(jugador, 1);
+		tablero.avanzarJugador(jugador, avanceDinamico.calcularAvance(jugador));
+		Visitable resultado = tablero.getPosicionDeJugador(jugador);
+		Assert.assertEquals(esperado, resultado);
+	}
+	
+	
 }
--- test/primeraEntregaTests/retrocesoDinamicoTest.java
+++ test/primeraEntregaTests/retrocesoDinamicoTest.java
@@ -1,7 +1,5 @@
 package primeraEntregaTests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +7,7 @@ import fiuba.algo3.Dado;
 import fiuba.algo3.Jugador;
 import fiuba.algo3.Tablero;
 import fiuba.algo3.celdas.Visitable;
+import fiuba.algo3.celdas.especiales.AvanceDinamico;
 import fiuba.algo3.celdas.especiales.RetrocesoDinamico;
 
 public class retrocesoDinamicoTest {
@@ -31,5 +30,24 @@ public class retrocesoDinamicoTest {
 		Visitable resultado = tablero.getPosicionDeJugador(jugador);
 		Assert.assertEquals(esperado, resultado);
 	}
+	@Test
+	public void jugadorCaeEnRetrocesoDinamicoSumando9SuPosicionDeberiaRetroceder1() {
+		Tablero tablero = new Tablero();
+		RetrocesoDinamico retrocesoDinamico = new RetrocesoDinamico();
+		Jugador jugador = new Jugador();
+		tablero.agregarCelda(retrocesoDinamico);
+		tablero.agregarJugador(jugador);
+		Dado dado1 = new Dado();
+		Dado dado2 = new Dado();
+		dado1.setValor(1);
+		dado2.setValor(8);
+		jugador.setValorDeTiro(dado1.getValor() + dado2.getValor());
+		Visitable esperado = tablero.getPosicionDeJugador(jugador);
+		tablero.avanzarJugador(jugador, 1);
+		tablero.avanzarJugador(jugador, retrocesoDinamico.calcularRetroceso(jugador));
+		Visitable resultado = tablero.getPosicionDeJugador(jugador);
+		Assert.assertEquals(esperado, resultado);
+	}
+	
 
 }
