c2ba1503a8334e61738a857e407ace20f08e1f5f
nazar9318 <fedelias93@gmail.com>
2017-12-06 01:27:53 -0300

    Agregada alerta cuando quiere construir demas en un barrio simple

--- src/fiuba/algo3/vista/eventos/BotonOpcionHandler.java
+++ src/fiuba/algo3/vista/eventos/BotonOpcionHandler.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.vista.eventos;
 
 import fiuba.algo3.modelo.Command.Command;
+import fiuba.algo3.modelo.excepciones.BarrioSimpleNoPuedeConstruirHotelException;
 import fiuba.algo3.modelo.excepciones.JugadorNoPuedeSalirDeLaCarcelException;
 import fiuba.algo3.modelo.excepciones.JugadorNoTieneFondosParaConstruirEnTerreno;
 import fiuba.algo3.modelo.Jugador;
@@ -40,6 +41,11 @@ public class BotonOpcionHandler extends BotonConSonido implements EventHandler<A
     		Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Mensaje de informacion");
             alert.setHeaderText("No tiene fondos para construir su edificacion!");
+            alert.show();
+    	}catch (BarrioSimpleNoPuedeConstruirHotelException e){
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Mensaje de informacion");
+            alert.setHeaderText("No puede construir hoteles en este barrio");
             alert.show();
     	}
     	
